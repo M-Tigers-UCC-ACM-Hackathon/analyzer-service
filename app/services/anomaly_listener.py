a916d9e7e2b3be5c55ea8e4942071fe4adff76f9
Henry-0810 <ml2002study@gmail.com>
2025-05-17 14:16:27 +0100

    listener done

---
+++ app/services/anomaly_listener.py
@@ -0,0 +1,43 @@
+import psycopg2
+import select
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_CONFIG = {
+    "host": os.getenv("PG_HOST"),
+    "port": os.getenv("PG_PORT"),
+    "user": os.getenv("PG_USER"),
+    "password": os.getenv("PG_PASSWORD"),
+    "dbname": os.getenv("PG_DB"),
+}
+# from app.services.anomaly_log import detect_anomalies
+# from app.db import get_connection
+
+
+def listen_for_new_logs():
+    # Ensure the trigger is set up
+    conn = psycopg2.connect(**DB_CONFIG, sslmode="require")
+    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute("LISTEN new_nginx_log;")
+    print("Listening for new logs...")
+
+    while True:
+        if select.select([conn], [], [], 5) == ([], [], []):
+            continue
+        conn.poll()
+        while conn.notifies:
+            notify = conn.notifies.pop(0)
+            new_id = int(notify.payload)
+            print(f"New log row inserted: {new_id}")
+            # detect_anomalies(new_id)  # your logic for preprocessing + 3 checks
+
+
+def main():
+    listen_for_new_logs()
+
+
+if __name__ == "__main__":
+    main()
