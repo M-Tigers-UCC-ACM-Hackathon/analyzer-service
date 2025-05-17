21ada50679cadb7bed34b0c9760ae0b93916bb1c
Henry-0810 <ml2002study@gmail.com>
2025-05-17 14:45:26 +0100

    queries done

---
+++ app/models/queries.py
@@ -0,0 +1,60 @@
+from psycopg2.extras import RealDictCursor
+
+
+def insert_analyzed_log(conn, log_row, flag=0):
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            INSERT INTO nginx_logs_analyzed (ip, log_time, method, path, http_ver, status, bytes, user_agent, flag)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """,
+            (
+                log_row["ip"],
+                log_row["log_time"],
+                log_row["method"],
+                log_row["path"],
+                log_row["http_ver"],
+                log_row["status"],
+                log_row["bytes"],
+                log_row["user_agent"],
+                flag,
+            ),
+        )
+        conn.commit()
+
+
+def update_analyzed_flag(conn, log_id, flag):
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            UPDATE nginx_logs_analyzed SET flag = %s WHERE id = %s
+        """,
+            (flag, log_id),
+        )
+        conn.commit()
+
+
+def insert_traffic_metrics(
+    conn, start_time, end_time, ip, total_requests, error_rate, flag
+):
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            INSERT INTO traffic_metrics (start_time, end_time, ip, total_requests, error_rate, flag)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """,
+            (start_time, end_time, ip, total_requests, error_rate, flag),
+        )
+        conn.commit()
+
+
+def insert_alert(conn, alert_type, severity, offender_ip, reason, explanation):
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            INSERT INTO alerts (alert_type, severity, offender_ip, reason, explanation)
+            VALUES (%s, %s, %s, %s, %s)
+        """,
+            (alert_type, severity, offender_ip, reason, explanation),
+        )
+        conn.commit()
